72a13de3899472bed2cbfd45f979047727db14e5
technic1 <dps_96@mail.ru>
2019-11-08 19:17:21 +0300

    adding mixin ObjectCreateMixin

--- blogengine/blog/utils.py
+++ blogengine/blog/utils.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 
 from .models import *
 
@@ -11,3 +13,18 @@ class ObjectDetailMixin:
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower(): obj})
 
+class ObjectCreateMixin():
+    model_form = None
+    template = None
+
+    def get(self, request):
+        form = self.model_form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.model_form(request.POST)
+
+        if bound_form.is_valid():
+            new_object = bound_form.save()
+            return redirect(new_object)
+        return render(request, self.template, context={'form': bound_form})

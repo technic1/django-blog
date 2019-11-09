e9cfa9ebcc63812a30e644b68857f112b976efb5
technic1 <dps_96@mail.ru>
2019-11-09 23:50:39 +0300

    tag and post update form with object update mixin

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import redirect
 from django.views.generic import View
 
 
-from .models import Post, Tag
 from .utils import *
 from .forms import TagForm, PostForm
 
@@ -21,18 +20,10 @@ class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
 
-    # def get(self, request):
-    #     form = PostForm()
-    #     return render(request, 'blog/post_create_form.html', context={'form': form})
-    #
-    # def post(self, request):
-    #     bound_form = PostForm(request.POST)
-    #
-    #     if bound_form.is_valid():
-    #         new_post = bound_form.save()
-    #         return redirect(new_post)
-    #     return render(request, 'blog/post_create_form.html', context={'form': bound_form})
-
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -42,18 +33,24 @@ class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
 
-    # def get(self, request):
-    #     form = TagForm()
-    #     return render(request, 'blog/tag_create.html', context={'form': form})
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance=tag)
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
     #
-    # def post(self, request):
-    #     bound_form = TagForm(request.POST)
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance=tag)
     #
-    #     if bound_form.is_valid():
+    #     if bound_form.is_valid:
     #         new_tag = bound_form.save()
     #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
-
+    #     return render(request, 'blog/tag_update_form', context={'form': bound_form, 'tag': tag})
 
 def tags_list(request):
     tags = Tag.objects.all()
